db0d0432215467133b0194c3fdb20293c012c5b6
jamesson12 <64281676+jamesson12@users.noreply.github.com>
2020-11-09 23:49:26 +0100

    Update chrooted_cleaner_script.sh

--- chrooted_cleaner_script.sh
+++ chrooted_cleaner_script.sh
@@ -116,7 +116,6 @@ _clean_archiso(){
         /etc/initcpio
         /etc/udev/rules.d/81-dhcpcd.rules
         /usr/bin/{calamares_switcher,cleaner_script.sh}
-        /home/$NEW_USER/.config/qt5ct
         /home/$NEW_USER/{.xinitrc,.xsession,.xprofile}
         /root/{.xinitrc,.xsession,.xprofile}
         /etc/skel/{.xinitrc,.xsession,.xprofile}
